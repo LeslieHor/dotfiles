49e64f66727b89d28dd2959ec7bdb50b1c2c24ef
Leslie Hor <Leslie.Hor@Gmail.com>
2021-02-28 10:20:30 +0000

    Add keyboard indicators

--- octavius/.config/keebie/scripts/switch-to-headset.sh
+++ octavius/.config/keebie/scripts/switch-to-headset.sh
@@ -1,2 +1,3 @@
 switch-audio-device output headset
 switch-audio-device input headset
+notify-send "Audio set to headset"
--- octavius/.config/keebie/scripts/switch-to-speakers.sh
+++ octavius/.config/keebie/scripts/switch-to-speakers.sh
@@ -1,2 +1,3 @@
 switch-audio-device output speakers
 switch-audio-device input webcam
+notify-send "Audio set to speakers"
